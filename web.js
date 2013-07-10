a0d97fb6b1b30e6191ac3eb8ce254a222c344ccd
Ubuntu <ubuntu@ip-172-31-18-182.us-west-2.compute.internal>
2013-07-10 02:06:09 +0000

    web.js without buffer

--- web.js
+++ web.js
@@ -1,15 +1,10 @@
 var fs = require('fs');
-var fdata = fs.readFileSync('index.html', 'utf8');
-var max_len = Buffer.byteLength(fdata, 'utf8');
-var buf = new Buffer(max_len);
-var disp = buf.toString('utf-8');
-console.log(disp);
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(disp);
+  response.send(fs.readFileSync('index.html', 'utf-8'));
 });
 
 var port = process.env.PORT || 5000;
